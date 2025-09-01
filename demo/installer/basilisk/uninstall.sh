5c1cc8be356dba26a1bc3a1fea2c2f49f1554f6e
samwhelp <samwhelp@users.noreply.github.com>
2025-09-01 19:56:12 +0800

    fix

---
+++ demo/installer/basilisk/uninstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+
+
+rm -rf "${HOME}/.local/share/basilisk"
+
+
+sudo rm -f "/usr/bin/basilisk"
+
+
+sudo rm -f "/usr/share/applications/basilisk.desktop"
---
+++ demo/installer/palemoon/uninstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+
+
+rm -rf "${HOME}/.local/share/palemoon"
+
+
+sudo rm -f "/usr/bin/palemoon"
+
+
+sudo rm -f "/usr/share/applications/palemoon.desktop"
